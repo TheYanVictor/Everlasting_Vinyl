54a760892a64c756b5a403e87b663b9319d510e5
Yan Victor <106403574+TheYanVictor@users.noreply.github.com>
2024-06-26 13:18:35 -0300

    footer e header devidamente separados

---
+++ js/footer.js
@@ -0,0 +1,18 @@
+// Essa função window.onload é para garantir que o header seja carregado depois que o HTML for carregado
+window.onload = function () {
+    let footer_template = `
+        <!-- Rodapé do site -->
+<footer>
+    <section class="name_footer">
+        <p>Everlasting Vinyl</p>
+    </section>
+    <section class="icones_footer">
+        <a href="#"><img id="icones_footer_logo"src="/assets/img/facebook.png" alt="Facebook"></a>
+        <a href="#"><img id="icones_footer_logo" src="/assets/img/instagram.png" alt="Instagram"></a>
+        <a href="#"><img id="icones_footer_logo" src="/assets/img/linkedin.png" alt="Linkedin"></a>
+        <a href="https://github.com/TheYanVictor/Everlasting_Vinyl"><img id="icones_footer_logo" src="/assets/img/github.png" alt="Github"></a>
+    </section>
+</footer>
+    `;
+    document.getElementById("general_footer").innerHTML = footer_template;
+};
---
+++ js/header.js
@@ -0,0 +1,51 @@
+// Essa função window.onload é para garantir que o header seja carregado depois que o HTML for carregado
+// window.onload = function() {
+    let header_template = `
+        <!-- Cabeçalho do site -->
+        <!-- O header é composto de 4 partes principais: Título, busca, navegação e carrinho/login -->
+        <header>
+            <!-- Brand name -->
+            <section class="upper_header">
+                <h1>Everlasting Vinyl</h1>
+                <p>O melhor dos mundos para os amantes de um bom som</p>
+            </section>
+            <br>
+            <hr style="width: 50%; margin: auto;">
+            <!-- Parte de baixo do header -->
+            <section class="lower_header">
+                <!-- Left section -->
+                <section class="col-1">
+                    <input type="text" id="barra_pesquisa" placeholder="Pesquisar...">
+                    <button id="botao_pesquisa_header"> <i class="fa fa-search"></i></button>
+                </section>
+                <!-- Navigation section -->
+                <section class="navbar">
+                    <nav>
+                        <ul>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Produtos</a></li>
+                            <li><a href="#">Sobre</a></li>
+                            <li><a href="#">Contato</a></li>
+                        </ul>
+                    </nav>
+                </section>  
+                <!-- Right section -->
+                <section class="right_section_header">
+                    <div class="cart">
+                        <a href="../templates/cart.html" style="color: white;">
+                            <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+                            <p>Carrinho (<span id="cart-count">0</span>)</p>
+                        </a>
+                    </div>
+                    <div class="loginSection">
+                        <a href="./login.html">
+                            <i class="fa fa-user-circle-o" aria-hidden="true"></i>
+                            <p>Login/cadastro</p>
+                        </a>
+                    </div>
+                </section>
+            </section>
+        </header>
+    `;
+    document.getElementById("general_header").innerHTML = header_template;
+// };
